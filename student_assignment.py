67ac5f497495e452786bee9ed36072cc17769a71
meilong_bao <Meilong_Bao@asus.com>
2025-01-17 14:53:57 +0800

    hw1
    Change-Id: Ibf32b3975698ab32c33df941924851de7717691c

--- student_assignment.py
+++ student_assignment.py
@@ -5,13 +5,62 @@ from model_configurations import get_model_configuration
 
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, FewShotChatMessagePromptTemplate
+
 
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
+
+def init_llm():
+    llm = AzureChatOpenAI(
+        model=gpt_config['model_name'],
+        deployment_name=gpt_config['deployment_name'],
+        openai_api_key=gpt_config['api_key'],
+        openai_api_version=gpt_config['api_version'],
+        azure_endpoint=gpt_config['api_base'],
+        temperature=gpt_config['temperature']
+    )
+    return llm
+
+
 def generate_hw01(question):
-    pass
-    
+    llm = init_llm()
+    examples = [
+        {
+            "input": "2024年台湾10月有哪些纪念日?",
+            "output": {
+                    "Result": [
+                        {"date": "2024-10-09", "name": "重阳节"},
+                        {"date": "2024-10-10", "name": "中华民国国庆日"}
+                    ]
+            }
+        }
+    ]
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{output}"),
+        ]
+    )
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt=example_prompt,
+        examples=examples,
+    )
+    # print few_shot_prompt
+    # print(few_shot_prompt.invoke({}).to_messages())
+    final_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "You are a Taiwan local assistant!"
+                       "You should follow the examples to give me the result in json format."),
+            few_shot_prompt,
+            ("human", "{input}"),
+        ]
+    )
+    chain = final_prompt | llm
+    response = chain.invoke({"input": question})
+    print(response.content)
+
 def generate_hw02(question):
     pass
     
@@ -38,3 +87,8 @@ def demo(question):
     response = llm.invoke([message])
     
     return response
+
+
+if __name__ == '__main__':
+    question = "2024年10月台湾有什么纪念日?"
+    generate_hw01(question)
